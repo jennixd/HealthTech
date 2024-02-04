c0fc929ac8b2c7d6a9f733946cb0339438b7796a
looihuihui1212 <s10255453@connect.np.edu.sg>
2024-02-04 17:31:58 +0800

    added in new icon image and 4 sections for index page

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function toggleSubmenu(submenuId) {
         submenu.style.display = 'block';
     }
 }
+
+// Signup form
+function handleSignupForm(event) {
+    event.preventDefault();
+    // Check if successful (replace with your actual logic)
+    var isSignupSuccessful = true; 
+    if (isSignupSuccessful) {
+        // Get the entered username
+        var username = document.getElementById('username').value;
+
+        // Update the navigation menu with the image (replace 'path/to/image.jpg' with the actual path)
+        var signupNavItem = document.getElementById('signupNavItem');
+        signupNavItem.innerHTML = `<a class="nav-link" href="profile.html"><img src="assets\\icons\\pfp.png" alt="${username}" style="width: 60px; height: 60px; border-radius: 50%;"></a>`;
+
+        // Close the modal
+        $('#signupModal').modal('hide');
+    }
+}
